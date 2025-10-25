4c4a3817a6d120eb9711a629e8b06d63f096a957
Evan Harrison <esharri2@gmail.com>
2025-10-25 12:45:51 -0400

    config update

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,9 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("**/*.css");
 	eleventyConfig.addWatchTarget("**/*.css");
+	eleventyConfig.addPlugin(HtmlBasePlugin, {baseHref: eleventyConfig.pathPrefix,});
 };
+
+
