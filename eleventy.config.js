73516058883147c181fb204ba09a1ee3601adaed
Evan Harrison <esharri2@gmail.com>
2025-11-08 14:29:28 -0500

    lazy load video example

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,24 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
 
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
+  console.log("yo: ", eleventyConfig.pathPrefix);
   eleventyConfig.addPassthroughCopy("**/*.css");
-	eleventyConfig.addWatchTarget("**/*.css");
-	eleventyConfig.addPlugin(HtmlBasePlugin, {baseHref: eleventyConfig.pathPrefix,});
-};
+  eleventyConfig.addPassthroughCopy("**/*.js");
+  eleventyConfig.addPassthroughCopy("**/assets/*");
 
 
+  eleventyConfig.addWatchTarget("**/*.css");
+  eleventyConfig.addWatchTarget("**/*.js");
+
+  eleventyConfig.addPlugin(HtmlBasePlugin, {
+    // The base URL: defaults to Path Prefix
+    baseHref: eleventyConfig.pathPrefix || "/",
+
+    // But you could use a full URL here too:
+    // baseHref: "http://example.com/"
+
+    // Comma separated list of output file extensions to apply
+    // our transform to. Use `false` to opt-out of the transform.
+    extensions: "html",
+  });
+}
